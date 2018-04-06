5d8c52dbd86d72694f8c16d2287e6c2fed10db9f
joeljvari <joeljvari@gmail.com>
2018-04-06 15:50:35 +0530

    Changed the Values,Added activity_main,Added activity_order

--- app/src/main/java/com/example/joeljohn/icafapp/MainActivity.java
+++ app/src/main/java/com/example/joeljohn/icafapp/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.joeljohn.icafapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +14,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /**
+     * Handles app bar item clicks.
+     *
+     * @param item Item clicked.
+     * @return Returns true if one of the defined items was clicked.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message,
@@ -47,4 +82,5 @@ public class MainActivity extends AppCompatActivity {
     public void showFroyoOrder(View view) {
         showFoodOrder(getString(R.string.froyo_order_message));
     }
+
 }
